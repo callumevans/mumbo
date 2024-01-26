fc7f9e5204421451d48755bf6971a535b420b85f
Callum Evans <callumevans@outlook.com>
2024-01-26 03:10:09 +0000

    Vite externals

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import vitePluginExternal from "vite-plugin-external";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -8,5 +10,16 @@ export default defineConfig({
         dts({
             exclude: "./src/Mumbo.spec.ts",
         }),
+        vitePluginExternal({
+            nodeBuiltins: true,
+        }),
     ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/Mumbo.ts"),
+            name: "Mumbo",
+            fileName: "Mumbo",
+            formats: ["cjs"],
+        },
+    },
 });
