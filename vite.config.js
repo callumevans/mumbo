c0b56cc66ee4633773d3b3eaf8ec1660a3cf8bc7
Callum Evans <callumevans@outlook.com>
2024-01-27 01:09:24 +0000

    Package dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import path from "node:path";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { normalizePath } from "vite";
 import dts from "vite-plugin-dts";
 import vitePluginExternal from "vite-plugin-external";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -13,8 +16,17 @@ export default defineConfig({
         vitePluginExternal({
             nodeBuiltins: true,
         }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: normalizePath(path.resolve(__dirname, "./package.json")),
+                    dest: "./",
+                },
+            ],
+        }),
     ],
     build: {
+        minify: false,
         lib: {
             entry: resolve(__dirname, "src/Mumbo.ts"),
             name: "Mumbo",
