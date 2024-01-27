2e5a153d42cbd816ec13ef911142e7562d35e3c4
Callum Evans <callumevans@outlook.com>
2024-01-27 01:04:49 +0000

    Linting

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+import path from "node:path";
 import { resolve } from "path";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
+import { normalizePath } from "vite";
 import dts from "vite-plugin-dts";
 import vitePluginExternal from "vite-plugin-external";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { normalizePath } from 'vite'
-import path from 'node:path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     plugins: [
@@ -19,11 +19,11 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {
-                    src: normalizePath(path.resolve(__dirname, './package.json')),
-                    dest: './'
-                }
-            ]
-        })
+                    src: normalizePath(path.resolve(__dirname, "./package.json")),
+                    dest: "./",
+                },
+            ],
+        }),
     ],
     build: {
         minify: false,
