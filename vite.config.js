ac31d4326883eda7ba98ecc3a0a8b0c39cb9ea2a
Callum Evans <callumevans@outlook.com>
2024-01-30 00:42:32 +0000

    Publish readme

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,10 @@ export default defineConfig({
                     src: normalizePath(path.resolve(__dirname, "./package.json")),
                     dest: "./",
                 },
+                {
+                    src: normalizePath(path.resolve(__dirname, "./README.md")),
+                    dest: "./",
+                },
             ],
         }),
     ],
